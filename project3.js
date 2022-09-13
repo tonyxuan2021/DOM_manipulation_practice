ecc2552ff63606b739a96b2d9c48c5b75199d800
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-13 11:26:46 -0700

    complete accordion feature

---
+++ project3.js
@@ -0,0 +1,10 @@
+// variable
+
+const accordions = document.querySelectorAll(".content-container");
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", function () {
+    console.log(this);
+    this.classList.toggle("active");
+  });
+});
